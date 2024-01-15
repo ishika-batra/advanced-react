e5e8ebc346890984d019bcf2f5c80b06e786eb2b
ishika2203 <ishikabatra.net@gmail.com>
2024-01-15 17:17:41 +0530

    use state object example added

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,26 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: 'Ishika',
+    age: 22,
+    hobby: 'Swim',
+  });
+
+  const displayPerson = () => {
+    setPerson({ name: 'Durgesh', age: 22, hobby: 'code' });
+  };
+
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3> Enjoys To: {person.hobby}</h3>
+      <button className="btn" onClick={displayPerson}>
+        Show Durgesh
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
