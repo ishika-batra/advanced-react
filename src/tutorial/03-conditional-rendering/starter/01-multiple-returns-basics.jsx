df130f38d1fd889c2ad09771f05b45143b3290ca
ishika2203 <ishikabatra.net@gmail.com>
2024-01-21 20:27:10 +0530

    multiple return basic added

--- src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
+++ src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
@@ -1,6 +1,18 @@
 import { useEffect, useState } from 'react';
 
 const MultipleReturnsBasics = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      // done fetching data
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
+  if (isLoading) {
+    return 'Loading...';
+  }
   return <h2>Multiple Returns Basics</h2>;
 };
 export default MultipleReturnsBasics;
