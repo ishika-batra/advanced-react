a88488194aebb5c476dffc8518979f2fcc52f9f1
ishika2203 <ishikabatra.net@gmail.com>
2024-01-23 23:42:41 +0530

    ternary operators added

--- src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
+++ src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
@@ -6,7 +6,7 @@ const ShortCircuitExamples = () => {
   // truthy
   const [name, setName] = useState('susan');
   const [user, setUser] = useState({ name: 'john' });
-  const [isEditing, setIsEditing] = useState(false);
+  const [isEditing, setIsEditing] = useState(true);
 
   return (
     <div>
@@ -24,6 +24,17 @@ const ShortCircuitExamples = () => {
         </div>
       )} */}
       {user && <SomeComponent name={user.name} />}
+      <h2 style={{ margin: '1rem 0' }}>Ternary Operator</h2>
+      <button className="btn">{isEditing ? 'edit' : 'add'}</button>
+      {user ? (
+        <div>
+          <h4>Hello There user: {user.name}</h4>
+        </div>
+      ) : (
+        <div>
+          <h4>please login</h4>
+        </div>
+      )}
     </div>
   );
 };
