980cd7cf85f2930930b3d87a207c7484ae861baa
ishika2203 <ishikabatra.net@gmail.com>
2024-01-24 01:00:01 +0530

    use effect timer example added

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,36 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [toggle, setToggle] = useState(false);
+  console.log('render');
+  return (
+    <div>
+      <button
+        className="btn"
+        onClick={() => {
+          setToggle(!toggle);
+        }}
+      >
+        toggle component
+      </button>
+      {toggle && <RandomComponent />}
+    </div>
+  );
+};
+
+const RandomComponent = () => {
+  useEffect(() => {
+    // console.log('use effect');
+    const intId = setInterval(() => {
+      // console.log('interval');
+    }, 1000);
+    return () => {
+      clearInterval(intId);
+      console.log('cleanup');
+    };
+  }, []);
+  return <h1>Hello</h1>;
 };
 
 export default CleanupFunction;
